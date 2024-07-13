83e9e03c4a68ef345535bfafe38061ada7c5ed84
Riyyi <riyyi3@gmail.com>
2024-07-13 19:07:07 +0200

    Add secrets.go file

---
+++ src/secrets.go
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2024 Riyyi
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package src
+
+var username string = "" // email
+var password string = "" // API key
+
+var base_url string = "" // base URL
